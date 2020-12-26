ab05ea2934005fd2856ff6ecdfb894d677b94d36
mattj-git <76628454+mattj-git@users.noreply.github.com>
2020-12-26 17:12:30 -0500

    Update Working with strings.c

--- Working with strings.c
+++ Working with strings.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Questions.h"
 
-void letter_freq(const char word[], int freq[]){
+void letter_freq(const char word[], int freq[]){  //This functions takes in a string and returns a string "freq" which returns displays how many times each letter appeared in the word
+						 //The array "freq" contains integers in the position respective to that letters position in the alphabet (ie. a/A is index 0, b/B is index 1). 
+	
 	int i = 0; 		                               // i is used to iterate through each entry of the string
 	int letter = 0;                                //This variable iterates through ASCII letters
 	while (word[i] != '\0')   //While loop which repeats until it hits the null character at the end of the string
@@ -20,3 +21,14 @@ void letter_freq(const char word[], int freq[]){
 
    
 }
+
+void string_copy(const char source[], char destination[], int n){ //This  functions copies the input string "source" to the output string "destination"
+
+	int i = 0;                    //Used to iterate through the entire array
+	for(i = 0; i < n-1; i++)      //Goes through the entire array except for the very last character (reserved for the null character)
+	{
+		destination[i] = source[i];   //Copies each entry from source to destination
+	}
+	destination[n-1] = '\0';          //Adds the null character to the end of each string
+}
+
