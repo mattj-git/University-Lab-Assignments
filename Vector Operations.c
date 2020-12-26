822c178e030a9a96ff234059c32f6a5641138232
mattj-git <76628454+mattj-git@users.noreply.github.com>
2020-12-26 15:38:30 -0500

    Update Vector Operations.c

--- Vector Operations.c
+++ Vector Operations.c
@@ -50,3 +50,38 @@ double norm2(double vector1[], int size) //This function takes the L2 norm of th
 	
     return L2;
 }
+
+void efficient(const int source[], int val[], int pos[], int size){
+
+	int i = 0;                    // Variable used to iterate through each item in the input array
+	int output_index = 0;              // used to iterate through the output arrays
+	for(i = 0; i < size; i++)     //Loop goes through the array
+	{
+		if (source[i] != 0)       // if statement which excludes zero entries
+		{
+			val[output_index] = source[i];     //If the entry is non zero it adds its position and value to their respective output arrays
+			pos[output_index] = i;
+			output_index += 1;                 //Only counts up in the output index if an entry is added to the output arrays
+		}
+	}
+	
+
+}
+
+void reconstruct(int source[], int m, const int val[], const int pos[], int n){
+
+	int i = 0;                     //Variable that iterates through the entire input array
+	int output_index = 0;          //Variable used to iterate through the output array
+	for (i = 0; i < m; i++ )    //Loop goes through the entire input array
+	{
+		if(i < pos[output_index])      //This if statement just detects if its at a place in the array before/between values (specifically the value at val[output_index]) and if so it places a 0 there
+		{
+			source[i] = 0;
+		}
+		else                           //This else statement takes into account if its at a place in the array where a value should be and places that value there (Same index as before)
+		{
+			source[i] = val[output_index];
+			output_index += 1;         //Increments the index used to go through the "val" and "pos" arrays
+		}
+	}
+}
