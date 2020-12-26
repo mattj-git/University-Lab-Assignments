647f3586ff4fb7374b3e257d5742b4298ba4b8a4
mattj-git <76628454+mattj-git@users.noreply.github.com>
2020-12-26 15:15:37 -0500

    Update Vector Operations.c

--- Vector Operations.c
+++ Vector Operations.c
@@ -5,15 +5,11 @@
 #include "Questions.h"
 
 
-
-void add_vectors(double vector1[],double vector2[],double vector3[],int size)
+void add_vectors(double vector1[],double vector2[],double vector3[],int size)  //This function adds 2 vectors and stores the sum in vector 3, given that both vectors have the same size
 {
-    /*vector3 should store the sum of vector1 and vector2. 
-	You may assume that all three arrays have the size equal to n
-	*/
-	//write your code below
+   
 	int i = 0;
-	double *vector1Ptr = &vector1[0];  //This and next 2 Lines just used to initialize pointers for each input array
+	double *vector1Ptr = &vector1[0];  //This and the next 2 lines are just used to initialize pointers for each input array
 	double *vector2Ptr = &vector2[0];
 	double *vector3Ptr = &vector3[0];
 
@@ -25,12 +21,11 @@ void add_vectors(double vector1[],double vector2[],double vector3[],int size)
 	
 }
 
-double scalar_prod(double vector1[],double vector2[],int size)
+double scalar_prod(double vector1[],double vector2[],int size)  //This function returns the scalar product of the 2 vectors, assuming they have the same size
 {
 	// this is the variable holding the scalar product of the two vectors
     double prod=0;
 
-	//write your code below to calculate the prod value
     double *vector1Ptr = &vector1[0];  //This and the next line just used to initialize pointers for each input array
     double *vector2Ptr = &vector2[0];
     int i = 0;
@@ -41,21 +36,17 @@ double scalar_prod(double vector1[],double vector2[],int size)
     
 
 	
-	// return the result
     return prod;
 }
 
-double norm2(double vector1[], int size)
+double norm2(double vector1[], int size) //This function takes the L2 norm of the given vector (the square root of the scalar product of the vector with itself)
 {
 	//this is the variable holding the L2 norm of the passed vector
     double L2;
 	
     L2 = pow(scalar_prod(vector1,vector1,size),0.5);   //This just takes the square root of the scalar product of the vector with itself
 
-    
-	//write your code here
-	// you should call function scalar_prod().
+   
 	
-	//finally, return the L2 norm 
     return L2;
 }
