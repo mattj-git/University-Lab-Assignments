33a572c189c67979833df8cc872829b020a51739
mattj-git <76628454+mattj-git@users.noreply.github.com>
2020-12-26 19:38:49 -0500

    Update Linked List Node (class).java

--- Linked List Node (class).java
+++ Linked List Node (class).java
@@ -1,10 +1,12 @@
 
-class SLLNode {
-    int value;
-    SLLNode next;
+class SLLNode { //This is used to  implement a linked list as a class, and each entry/ "node" is defined as this class
+    int value;       //In a linked list each entry has avalue represented by this instance variable
+    SLLNode next;   //This instance variable is a pointer pointing to the next node/entry in the linked list
 
-    SLLNode(int i, SLLNode n) {
+    SLLNode(int i, SLLNode n) {  //This method is a constructor for the node.
         this.value = i;
         this.next = n;
     }
 }
+
+//There's not much in this class file but this class is used to implement the linked list class in the "linked list (class)" file
